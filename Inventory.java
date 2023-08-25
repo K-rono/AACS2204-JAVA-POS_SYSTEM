37b8e5464935b69c631c9a096bb3a11039ee8da4
K-rono <117577524+K-rono@users.noreply.github.com>
2023-08-25 21:29:59 +0800

    Add files via upload

---
+++ Inventory.java
@@ -0,0 +1,131 @@
+package POS;
+
+/**
+ *
+ * @author qihong
+ */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Inventory {
+    private static final String STOCK_FILE_PATH = "src/stock.txt";
+    private static final String PRODUCT_FILE_PATH = "src/product.txt";
+    private final List<Product> products;
+    private final Map<String, List<Product>> productsByCategory;
+    private final Map<Integer, Integer> productStock;
+    
+    public Inventory(){
+        products = new ArrayList<>();
+        productsByCategory = new HashMap<>();
+        productStock = new HashMap<>();
+    }
+    
+    public void readProductsFromFile(){
+        try(BufferedReader reader = new BufferedReader(new FileReader(PRODUCT_FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                int productID = Integer.parseInt(data[0]);
+                String productName = data[1];
+                String ratedAge = data[2];
+                String category = data[3];
+                double price = Double.parseDouble(data[4]);
+
+                Product product = new Product(productID, productName, ratedAge, category, price);
+                products.add(product);
+                productsByCategory.putIfAbsent(category, new ArrayList<>());
+                productsByCategory.get(category).add(product);
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error occured when reading products file");
+        }
+    }
+    
+    public void readStockFromFile(){
+        try(BufferedReader reader = new BufferedReader(new FileReader(STOCK_FILE_PATH))) {
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] data = line.split(",");
+                int productID = Integer.parseInt(data[0]);
+                int stock = Integer.parseInt(data[1]);
+                
+                productStock.put(productID, stock);
+            }
+        }
+        catch(IOException e){
+            System.out.println("Error occured when reading stocks file");
+        }
+    }
+    
+    public void writeProductsToFile(Product product){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(PRODUCT_FILE_PATH))){
+            writer.write(product.getProductID()
+                    + "," + product.getProductName()
+                    + "," + product.getRatedAge()
+                    + "," + product.getCategory()
+                    + "," + product.getPrice());
+            writer.newLine();
+        }
+        catch(IOException e){
+            System.out.println("Error occured when writing to products text file");
+        }
+    }
+    
+    public void writeStockToFile(int productID,int qty){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(STOCK_FILE_PATH))){
+            writer.write(productID + "," + qty);
+            writer.newLine();
+        }
+        catch(IOException e){
+            System.out.println("Error occured when writing to stocks text file");
+        }
+    }
+    
+    public void addNewProduct(String productName, String ratedAge, String category, double price){
+        Product product = new Product(productName,ratedAge,category,price);
+        productsByCategory.get(category).add(product);
+        Product.writeLastAssignedID();
+        writeProductsToFile(product);
+    }
+    
+    public void addStock(int productID, int qty){
+        if(productStock.containsKey(productID)){
+            int currentStock = productStock.get(productID);
+            productStock.put(productID, qty + currentStock);
+        }
+        else{
+            productStock.put(productID, qty);
+        }
+        
+        writeStockToFile(productID,productStock.get(productID));
+    }
+    
+    public List<Product> getProductList(String category){
+        return productsByCategory.get(category);
+    }
+    
+    public Set<String> getCategory(){
+        return productsByCategory.keySet();
+    }
+    
+    public Product getProduct(int productID){
+        for(Product product : products){
+            if(productID == product.getProductID()){
+                return product;
+            }
+        }
+        //productID not present
+        return null;
+    }
+    
+}
---
+++ Product.java
@@ -0,0 +1,111 @@
+package POS;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author qihong
+ */
+public class Product {
+    public static final String filePath = "src/lastAssignedID.txt";
+    public static int lastAssignedID = 1001;
+    private int productID;
+    private String productName;
+    private String ratedAge;
+    private String category;
+    private double price;
+
+    //Constructor for products that already exist in file
+    public Product(int productID, String productName, String ratedAge, String category, double price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.ratedAge = ratedAge;
+        this.category = category;
+        this.price = price;
+    }
+
+    //Constructor for newly added products
+    public Product(String productName, String ratedAge, String category, double price) {
+        this.productName = productName;
+        this.ratedAge = ratedAge;
+        this.category = category;
+        this.price = price;
+        this.productID = lastAssignedID;
+        lastAssignedID++;
+    }
+
+    //Setters
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setRatedAge(String ratedAge) {
+        this.ratedAge = ratedAge;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //Getters
+    public int getProductID() {
+        return productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getRatedAge() {
+        return ratedAge;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    //Read LastAssignedID
+    public static void readLastAssignedID() {
+        final String filePath = "src/lastAssignedID.txt";
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line = reader.readLine();
+            Product.lastAssignedID = Integer.parseInt(line);    
+        }
+        catch (IOException e){
+            System.out.println("Error occured when reading lastAssignedID text file");
+        }
+    }
+    
+    //Write LastAssignedID
+    public static void writeLastAssignedID(){
+        
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))){
+            writer.write(lastAssignedID);    
+        }
+        catch (IOException e){
+            System.out.println("Error occured when writing to lastAssignedID text file");
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%-10s %-30s %-5s %10s", productID, productName, ratedAge, price);
+    }
+
+}
