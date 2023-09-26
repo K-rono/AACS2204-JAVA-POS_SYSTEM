4e5c7545a300a37193edbbb7f255aead3092d66a
LIM QI HONG <117577524+K-rono@users.noreply.github.com>
2023-09-27 01:43:25 +0800

    Update InventoryAPI.java

--- InventoryAPI.java
+++ InventoryAPI.java
@@ -5,6 +5,8 @@ package POS;
  * @author Qihong
  */
 public interface InventoryAPI {
+    public ArrayList<Product> getProductList(String category);
+
     public abstract Product getProduct(int productID);
 
     public abstract int getStockAmount(int ProductID);
